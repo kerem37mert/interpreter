bb37cad464196441426f84c5f281fa01c4eed358
Kerem Mert <kerem37mert@yahoo.com>
2025-03-22 17:13:54 +0300

    error fonksiyonu oluşturuldu

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1,4 +1,5 @@
 #include "parser.hpp"
+#include <iostream>
 
 Parser::Parser(const std::vector<Token>& tokens)
     : tokens(tokens), currentToken(0), hasError(false) {}
@@ -55,4 +56,48 @@ Token Parser::previous() const {
 
 bool Parser::isAtEnd() const {
     return this->peek().type == TokenType::TOKEN_EOF;
+}
+
+void Parser::synchronize() {
+    this->advance();
+
+    while(!this->isAtEnd()) {
+        if(this->previous().type == TokenType::SEMI_COLON)
+            return;
+
+        switch(this->peek().type) {
+            case TokenType::VAR:
+            case TokenType::CONST:
+            case TokenType::FUNCTION:
+            case TokenType::IF:
+            case TokenType::LOOP:
+            case TokenType::RETURN:
+            case TokenType::PRINT:
+                return;
+            default:
+                break;
+
+            this->advance();
+        }
+    }
+}
+
+void Parser::error(const Token& token, const std::string& message) {
+    this->hasError = true;
+
+    std::cerr << "\n\033[1;31m========== SÖZDİZİMİ HATASI ==========\033[0m" << std::endl;
+
+    if (token.type == TokenType::TOKEN_EOF) {
+        std::cerr << "\033[1;31mDosya sonu: " << message << "\033[0m" << std::endl;
+    } else {
+        std::string tokenValue(token.start, token.length);
+        std::cerr << "\033[1;31mSözdizimi hatası: " << message << "\033[0m" << std::endl;
+        std::cerr << "Token: '" << tokenValue << "'" << std::endl;
+        std::cerr << "Dosya: " << token.fileName << std::endl;
+        std::cerr << "Satır: " << token.line << ", Sütun: " << token.column << std::endl;
+    }
+
+    std::cerr << "\033[1;31m=====================================\033[0m" << std::endl;
+
+    throw std::exception();
 }
