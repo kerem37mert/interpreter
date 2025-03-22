fb44e7547357456fde8d14c62f79ceb533ab637c
Kerem Mert <kerem37mert@yahoo.com>
2025-03-22 16:20:18 +0300

    parser sınıfı oluşturuldu

--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -8,34 +8,55 @@
 
 class Parser {
 public:
-    explicit Parser(std::vector<Token>& tokens);
-    std::vector<std::unique_ptr<Stmt>> parse();
+    explicit Parser(const std::vector<Token>& tokens);
+
+    std::unique_ptr<Program> parse();
 
 private:
-    std::vector<Token>& tokens;
-    unsigned int currentPosition;
+    std::vector<Token> tokens;
+    unsigned int currentToken;
+    bool hasError;
 
-    std::unique_ptr<Expression> parseExpression();
-    std::unique_ptr<Expression> asignment();
-    std::unique_ptr<Expression> logicOR();
-    std::unique_ptr<Expression> logicAND();
+    std::unique_ptr<Stmt> statement();
+    std::unique_ptr<Stmt> printStatement();
+    std::unique_ptr<Stmt> expressionStatement();
+    std::unique_ptr<Stmt> blockStatement();
+    std::unique_ptr<Stmt> ifStatement();
+    std::unique_ptr<Stmt> loopStatement();
+    std::unique_ptr<Stmt> breakStatement();
+    std::unique_ptr<Stmt> continueStatement();
+    std::unique_ptr<Stmt> returnStatement();
+    std::unique_ptr<Stmt> varDeclaration();
+    std::unique_ptr<Stmt> functionDeclaration();
+
+    std::unique_ptr<Expression> expression();
+    std::unique_ptr<Expression> assignment();
+    std::unique_ptr<Expression> logicalOr();
+    std::unique_ptr<Expression> logicalAnd();
     std::unique_ptr<Expression> equality();
     std::unique_ptr<Expression> comparison();
-    std::unique_ptr<Expression> addition();
-    std::unique_ptr<Expression> multiplication();
+    std::unique_ptr<Expression> term();
+    std::unique_ptr<Expression> factor();
+    std::unique_ptr<Expression> power();
+    std::unique_ptr<Expression> unary();
     std::unique_ptr<Expression> call();
     std::unique_ptr<Expression> primary();
+    std::unique_ptr<Expression> arrayExpression();
+    std::unique_ptr<Expression> finishCall(std::unique_ptr<Expression> callee);
+    std::unique_ptr<Expression> arrayAccess(std::unique_ptr<Expression> array);
 
-    Token& advance();
-    Token& peek();
-    Token& nextPeek();
-    Token& consumed();
+    Token advance();
+    bool check(TokenType type) const;
+    bool match(TokenType type);
+    bool match(const std::initializer_list<TokenType>& types);
+    Token consume(TokenType type, const std::string& message);
+    Token peek() const;
+    Token previous() const;
+    bool isAtEnd() const;
 
-    bool isMatch(TokenType type);
-
-    void consume(TokenType type, std::string& message);
-
-    void error(std::string& message);
+    void synchronize();
+    void error(const Token& token, const std::string& message);
 };
+
 
 #endif //PARSER_H
