2dc296d5ec703b71fde962cce08b399f71180d6a
Kerem Mert <kerem37mert@yahoo.com>
2025-04-21 13:00:21 +0300

    print getIndent implemente edildi

--- src/ast/astPrinter.cpp
+++ src/ast/astPrinter.cpp
@@ -1,2 +1,18 @@
 #include "astPrinter.hpp"
 
+void ASTPrinter::print(AstNode* node) {
+    node->accept(*this);
+}
+
+std::string ASTPrinter::getIndent() const {
+    std::string result;
+
+    for(int i=0; i<indentLevel; i++)
+        result += indent;;
+
+    return result;
+}
+
+void ASTPrinter::printNode(const std::string& name, const std::string& details) {
+    // yazılacak
+}
