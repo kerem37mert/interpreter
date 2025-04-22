a668e86cae0624cc67907dd7932ba26f9636ff13
Kerem Mert <kerem37mert@yahoo.com>
2025-04-22 10:23:56 +0300

    Printer için bazı visit metotları yazıldı

--- src/ast/astPrinter.cpp
+++ src/ast/astPrinter.cpp
@@ -24,4 +24,102 @@ void ASTPrinter::printNode(const std::string& name, const std::string& details) 
     std::cout << std::endl;
 }
 
-// Visitor Metotaları Yazılacak
+// Visitor Metotaları
+void ASTPrinter::visitBinaryExpression(BinaryExpression* expr) {
+    std::string op(expr->op.start, expr->op.length);
+    this->printNode("İkili ifade", "İşlem" + op);
+
+    this->indentLevel++;
+    std::cout << this->getIndent() << "Sol:" << std::endl;
+    this->indentLevel++;
+    expr->left->accept(*this);
+    this->indentLevel--;
+
+    std::cout << this->getIndent() << "Sağ:" << std::endl;
+    this->indentLevel++;
+    expr->right->accept(*this);
+    this->indentLevel--;
+    this->indentLevel--;
+}
+
+void ASTPrinter::visitLogicalExpression(LogicalExpression* expr) {
+    std::string op(expr->op.start(), expr->op.length);
+    this->printNode("Mantıksal İfade", "İşlem" + op);
+
+    this->indentLevel++;
+    std::cout << this->getIndent() << "Sol:" << std::endl;
+    this->indentLevel++;
+    expr->left->accept(*this);
+    this->indentLevel--;
+
+    std::cout <<  this->getIndent() << "Sağ:" << std::endl;
+    this->indentLevel++;
+    expr->right->accept(*this);
+    this->indentLevel--;
+    this->indentLevel--;
+}
+
+void ASTPrinter::visitUnaryExpression(UnaryExpression* expr) {
+    std::string op(expr->op.start, expr->op.length);
+    this->printNode("Tekli İfade", "İşlem" + op);
+
+    this->indentLevel++;
+    expr->operand->accept(*this);
+    this->indentLevel--;
+}
+
+void ASTPrinter::visitComparisonExpression(ComparisonExpression* expr) {
+    std::string op(expr->op.start, expr->op.length);
+    this->printNode("Karşılaştırma İfadesi", "İşlem" + op);
+
+    this->indentLevel++;
+    std::cout << this->getIndent() << "Sol:" << std::endl;
+    this->indentLevel++;
+    expr->left->accept(*this);
+    this->indentLevel--;
+
+    std::cout << this->getIndent() << "Sağ:" << std::endl;
+    this->indentLevel++;
+    expr->right->accept(*this);
+    this->indentLevel--;
+    this->indentLevel--;
+}
+
+void ASTPrinter::visitLiteralExpression(LiteralExpression* expr) {
+    std::string value = expr->value;
+    std::string type;
+
+    switch(expr->token.type) {
+        case TokenType::NUMBER_LITERAL:
+            type = "sayı";
+            break;
+        case TokenType::STRING_LITERAL:
+            type = "metin";
+            break;
+        case TokenType::TRUE:
+        case TokenType::FALSE:
+            type = "doğruluk";
+            break;
+        default:
+            type = "bilinmeyen";
+    }
+
+    this->printNode("Sabit Değer", "Değer" + value + ", Tip: " + type);
+}
+
+void ASTPrinter::visitVariableExpression(VariableExpression* expr) {
+    std::string name(expr->name.start, expr->name.length);
+    this->printNode("Değişken", "İsim: " + name);
+}
+
+void ASTPrinter::visitAssignExpression(AssignExpression* expr) {
+    std::string name(expr->name.start, expr->name.length);
+    this->printNode("Atama İfadesi", "Hedef: " + name);
+
+    this->indentLevel++;
+    std::cout << this->getIndent() << "Değer:" << std::endl;
+    this->indentLevel++;
+    expr->value->accept(*this);
+    this->indentLevel--;
+    this->indentLevel--;
+}
