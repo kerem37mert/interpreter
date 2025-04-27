ab6db31eec605f6e58220810b47dad2aeac7c591
Kerem Mert <kerem37mert@yahoo.com>
2025-04-27 11:29:28 +0300

    value dosyaları oluşturuldu

--- src/interpreter/interpreter.hpp
+++ src/interpreter/interpreter.hpp
@@ -1,4 +1,13 @@
 #ifndef INTERPRETER_HPP
 #define INTERPRETER_HPP
+#include "../ast/ast.hpp"
+
+class Interpreter : public ASTVisitor {
+public:
+    Interpreter();
+    void interpret(Program* program);
+    void runTimeError(const Token& token, const std::string& message);
+
+};
 
 #endif //INTERPRETER_HPP
---
+++ src/interpreter/value.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Kerem on 4/27/2025.
+//
+
+#ifndef VALUE_HPP
+#define VALUE_HPP
+
+#endif //VALUE_HPP
