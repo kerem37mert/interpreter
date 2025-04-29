9915e606c0a298a1ff9eafd58d9e83ed2941f41a
Kerem Mert <kerem37mert@yahoo.com>
2025-04-29 10:14:03 +0300

    hatalar giderildi

--- src/interpreter/interpreter.hpp
+++ src/interpreter/interpreter.hpp
@@ -6,7 +6,20 @@ class Interpreter : public ASTVisitor {
 public:
     Interpreter();
     void interpret(Program* program);
-    void runTimeError(const Token& token, const std::string& message);
+
+    void visitBinaryExpression(BinaryExpression* expr) override;
+    void visitLogicalExpression(LogicalExpression* expr) override;
+    void visitUnaryExpression(UnaryExpression* expr) override;
+    void visitComparisonExpression(ComparisonExpression* expr) override;
+    void visitLiteralExpression(LiteralExpression* expr) override;
+    void visitVariableExpression(VariableExpression* expr) override;
+    void visitAssignExpression(AssignExpression* expr) override;
+    void visitCallExpression(CallExpression* expr) override;
+    void visitArrayExpression(ArrayExpression* expr) override;
+    void visitArrayAccessExpression(ArrayAccessExpression* expr) override;
+    void visitArrayAssignExpression(ArrayAssignExpression* expr) override;
+
+    //void runTimeError(const Token& token, const std::string& message);
 };
 
 #endif //INTERPRETER_HPP
